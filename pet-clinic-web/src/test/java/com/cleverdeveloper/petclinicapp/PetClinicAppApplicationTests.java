a45cd6e8f786564bf8496087dac7794e503e881c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-21 00:17:11 +0100

    Setting up Project To Multi Module: (pet-clinic data & pet-clinic-web) ... closes #1. (Adding Maven Release plugin) ... closes #3.

---
+++ pet-clinic-web/src/test/java/com/cleverdeveloper/petclinicapp/PetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.petclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
