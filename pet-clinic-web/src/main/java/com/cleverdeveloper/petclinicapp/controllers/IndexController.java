5d3e6c81899b503da41ead4d116dcf282d092439
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-21 22:15:57 +0100

    Add Index Page & Controller. closes #8.

---
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.cleverdeveloper.petclinicapp.controllers;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/21/2021 10:12 PM
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
