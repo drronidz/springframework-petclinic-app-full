0b1d23c92dd60951e870b3ef9de3c22867a30f92
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-21 22:29:48 +0100

    Adding Vets Page & Controller. closes #14.

---
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/VetController.java
@@ -0,0 +1,22 @@
+package com.cleverdeveloper.petclinicapp.controllers;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/21/2021 10:25 PM
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+
+
+}
