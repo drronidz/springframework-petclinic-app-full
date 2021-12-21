78277dba4a24ae78a24d3e268e9d00daf143433c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-21 23:25:33 +0100

    Listing owners on Owners Page and Updating controller. closes #18.

--- pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/OwnerController.java
@@ -7,15 +7,26 @@ Author Name : @ DRRONIDZ
 DATE : 12/21/2021 10:31 PM
 */
 
+import com.cleverdeveloper.petclinicapp.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"" ,"/", "/index", "/index.html"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
