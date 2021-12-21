cce4a3c1ec94ba9116da95ce8468b4bc9e3bc8db
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-21 23:40:43 +0100

    Listing Vets on Vets Page and Updating controller. closes #17.

--- pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/VetController.java
@@ -7,14 +7,25 @@ Author Name : @ DRRONIDZ
 DATE : 12/21/2021 10:25 PM
 */
 
+import com.cleverdeveloper.petclinicapp.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listOfVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
