a18f0626d7849d91d2f4834e9c855c88d7aa08bd
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-22 15:31:52 +0100

    Creating PetType Map Service Implementation. closes #31.

---
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/services/PetTypeService.java
@@ -0,0 +1,14 @@
+package com.cleverdeveloper.petclinicapp.services;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/22/2021 3:27 PM
+*/
+
+import com.cleverdeveloper.petclinicapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+    
+}
---
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/services/map/PetTypeMapService.java
@@ -0,0 +1,42 @@
+package com.cleverdeveloper.petclinicapp.services.map;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/22/2021 3:28 PM
+*/
+
+import com.cleverdeveloper.petclinicapp.model.PetType;
+import com.cleverdeveloper.petclinicapp.services.PetService;
+import com.cleverdeveloper.petclinicapp.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType item) {
+        return super.save(item);
+    }
+
+    @Override
+    public void delete(PetType item) {
+        super.delete(item);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
