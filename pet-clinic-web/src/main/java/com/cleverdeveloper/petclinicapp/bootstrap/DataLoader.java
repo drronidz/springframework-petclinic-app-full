e44e8d2d8e2e6dba9fb70c4568e589bbb7d97bff
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-22 15:38:07 +0100

    Adding Pet Types in with Bootstrap (startup). closes #34.

--- pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/bootstrap/DataLoader.java
@@ -8,29 +8,40 @@ DATE : 12/21/2021 10:39 PM
 */
 
 import com.cleverdeveloper.petclinicapp.model.Owner;
-import com.cleverdeveloper.petclinicapp.model.Pet;
+import com.cleverdeveloper.petclinicapp.model.PetType;
 import com.cleverdeveloper.petclinicapp.model.Vet;
 import com.cleverdeveloper.petclinicapp.services.OwnerService;
+import com.cleverdeveloper.petclinicapp.services.PetTypeService;
 import com.cleverdeveloper.petclinicapp.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType toSaveDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType toSaveCat = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes ...");
 
         Owner ownerOne = new Owner();
         ownerOne.setFirstName("Tom");
