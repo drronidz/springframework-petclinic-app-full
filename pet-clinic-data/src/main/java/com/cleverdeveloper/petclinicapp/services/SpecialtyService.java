3a97e60dec8621713d4cfd9d07d8607a16151e97
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-22 15:57:45 +0100

    Creating Specialty Map Service Implementation. closes #33.

---
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/services/SpecialtyService.java
@@ -0,0 +1,13 @@
+package com.cleverdeveloper.petclinicapp.services;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/22/2021 3:54 PM
+*/
+
+import com.cleverdeveloper.petclinicapp.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/services/map/SpecialtyMapService.java
@@ -0,0 +1,40 @@
+package com.cleverdeveloper.petclinicapp.services.map;
+
+/*
+PROJECT NAME : pet-clinic-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/22/2021 3:54 PM
+*/
+
+import com.cleverdeveloper.petclinicapp.model.Specialty;
+import com.cleverdeveloper.petclinicapp.services.SpecialtyService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty item) {
+        super.delete(item);
+    }
+
+    @Override
+    public Specialty save(Specialty item) {
+        return super.save(item);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
