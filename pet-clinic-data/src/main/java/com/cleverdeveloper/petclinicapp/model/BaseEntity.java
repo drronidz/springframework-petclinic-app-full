9b951d63c12c16c98101f91210cf2b9f5bde121b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-23 12:44:58 +0100

    Implement Base Entity (JPA) -Update base entity with JPA Annotations .closes #37.

--- pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/model/BaseEntity.java
@@ -7,12 +7,19 @@ Author Name : @ DRRONIDZ
 DATE : 12/21/2021 2:24 PM
 */
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     // This is a baseEntity for global items ...
     //
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
