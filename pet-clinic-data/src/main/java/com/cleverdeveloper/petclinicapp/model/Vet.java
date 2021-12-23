57266e6f5284745284aa9cd145c63f9407eb0b53
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-23 13:03:20 +0100

    Convert Vets to JPA Entities.closes #39.

--- pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/model/Vet.java
+++ pet-clinic-data/src/main/java/com/cleverdeveloper/petclinicapp/model/Vet.java
@@ -7,11 +7,19 @@ Author Name : @ DRRONIDZ
 DATE : 12/21/2021 2:08 PM
 */
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable
+            (name = "vet_specialty",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
