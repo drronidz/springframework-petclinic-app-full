21f06292413e9ec42ec6a6d15611cde655e9973f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-24 13:55:05 +0100

    Adding WebDataBinder to interrupt the manipulation of Id property (via form) . #55

--- pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/OwnerController.java
@@ -10,7 +10,9 @@ DATE : 12/21/2021 10:31 PM
 import com.cleverdeveloper.petclinicapp.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,11 +27,15 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        // Manipulating Id property (via form) is not allowed (security stuffs)!
+        dataBinder.setDisallowedFields("id");
+    }
+
     @RequestMapping({"" ,"/", "/index", "/index.html"})
     public String listOfOwners(Model model) {
-
         model.addAttribute("owners", ownerService.findAll());
-
         return "owners/index";
     }
 
