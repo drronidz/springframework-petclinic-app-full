db30b7ee0551019ab58d74536be3e6fa742f9e3e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-26 22:50:56 +0100

    Implement Vets JSON Endpoint. closes #76.

--- pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/cleverdeveloper/petclinicapp/controllers/VetController.java
@@ -7,10 +7,15 @@ Author Name : @ DRRONIDZ
 DATE : 12/21/2021 10:25 PM
 */
 
+import com.cleverdeveloper.petclinicapp.model.Vet;
 import com.cleverdeveloper.petclinicapp.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -27,6 +32,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 
 
